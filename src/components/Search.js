900bd21413c74e2aaa828099cdc60b5088117f61
Jesse Quijano <jesse@quijano.net>
2020-07-28 15:43:30 -0700

    restructure debounce to remove eslint warning

--- src/components/Search.js
+++ src/components/Search.js
@@ -4,7 +4,19 @@ import axios from 'axios';
 const Search = () => {
 
     const [term, setTerm] = useState('programming');
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
     const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        };
+
+    }, [term]);
 
     useEffect(() => {
         const search = async () => {
@@ -14,27 +26,16 @@ const Search = () => {
                     list: 'search',
                     origin: '*',
                     format: 'json',
-                    srsearch: term
+                    srsearch: debouncedTerm
                 }
             });
 
             setResults(data.query.search);
         };
 
-        if (term && !results.length) {
-            search();
-        } else {
-            const timeoutId = setTimeout(() => {
-                if (term) {
-                    search();
-                }
-            }, 500);
-            
-            return ()  => {
-                clearTimeout(timeoutId);
-            };
-        }
-    }, [term]);
+        search();
+
+    }, [debouncedTerm]);
 
     const renderedResult = results.map((result) => {
         return (
